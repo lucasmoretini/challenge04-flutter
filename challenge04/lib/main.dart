f99c4d2bf378e75f84b6fc6aac215a4af4bec6ac
Luiz Henrique Cedraz <luiz.cedraz@picpay.com>
2022-10-11 22:56:03 -0300

    adding router

--- challenge04/lib/main.dart
+++ challenge04/lib/main.dart
@@ -1,6 +1,39 @@
-import 'package:challenge04/src/app/modules/main_page/main_page.dart';
+import 'package:challenge04/src/app/modules/intro_page/intro_page.dart';
 import 'package:flutter/material.dart';
 
+import 'src/theme/app_theme.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(TruckDriverApp());
+}
+
+class TruckDriverApp extends StatefulWidget {
+  const TruckDriverApp({Key? key}) : super(key: key);
+
+  @override
+  State<TruckDriverApp> createState() => _TruckDriverAppState();
+}
+
+class _TruckDriverAppState extends State<TruckDriverApp> {
+  @override
+  void initState() {
+    super.initState();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: lightTheme,
+      debugShowCheckedModeBanner: false,
+      routes: {
+        "/": (context) => IntroPage()
+      },
+    );
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+  }
 }
